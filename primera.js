0eaff298ecde4d339977683dbc2b66f89cf7b270
GGene29 <sumozacalcurian@gmail.com>
2023-08-07 16:52:06 -0400

    practica-jquery

--- primera.js
+++ primera.js
@@ -1,29 +1,73 @@
-document.getElementById('boton-plus').addEventListener("click", function(){
-    call_modal();
+
+
+
+console.log("Primera prueba jquery")  
+$(document).ready(function() {
+
+    $('body').on('click', '#boton-plus', function(){
+        console.log('hello')
+        $('#myModal').dialog({
+            modal:true,
+            
+        });
+    });
+
+    $('body').on('click', '#btnCerrarModal' ,function(){
+        console.log('hola')
+        $('#myModal').hide();
+    })
+    
+    // $("#myModal").dialog({
+    //     modal:true,
+    //     title: "Modal",
+    //     show: {
+    //         effect:"fade",
+    //         duration:1000,
+    //     },
+    //     button:[{
+    //         text: "Close",
+    //         close:true,
+    //     }],
+    //     $content: qweb.render("principal"),
+    // });
 });
 
-function call_modal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "block";
-}
-  
-function ocultarModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none";
-}
-  
-var btnCerrarModal = document.getElementById("btnCerrarModal");
-btnCerrarModal.addEventListener("click", ocultarModal);
-  
-var modal = document.getElementById("myModal");
-window.addEventListener("click", function(event) {
-if (event.target == modal) {
-    ocultarModal();
-}
-});
-  
 
 
+// document.getElementById('boton-plus').addEventListener("click", function(){
+//     call_modal();
+// });
+
+// function call_modal() {
+//     var modal = document.getElementById("myModal");
+//     modal.style.display = "block";
+// }
+  
+// function ocultarModal() {
+//     var modal = document.getElementById("myModal");
+//     modal.style.display = "none";
+// }
+  
+// var btnCerrarModal = document.getElementById("btnCerrarModal");
+// btnCerrarModal.addEventListener("click", ocultarModal);
+  
+// var modal = document.getElementById("myModal");
+// window.addEventListener("click", function(event) {
+// if (event.target == modal) {
+//     ocultarModal();
+// }
+// });
+
+
+    
+    // $('#myModal').dialog({
+    //     show: {
+    //         effect: "slide",
+    //         duration: 1000,
+    //     },
+    //     wigth: 500,
+    //     heigth: 500,
+    // });
 
 // primera = 'Holaaaa';
 // console.log('Primera');
